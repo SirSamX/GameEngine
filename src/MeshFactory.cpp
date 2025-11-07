3b50b55668f1eafb90f69189c7e72a7b2223d882
SirSam <115340828+SirSamX@users.noreply.github.com>
2025-11-07 20:51:06 +0100

    Add MeshFactory

---
+++ src/MeshFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by samuel on 11/7/25.
+//
+
+#include "MeshFactory.h"
---
+++ src/MeshFactory.h
@@ -0,0 +1,13 @@
+//
+// Created by samuel on 11/7/25.
+//
+
+#ifndef OPENGLDEMO_MESHFACTORY_H
+#define OPENGLDEMO_MESHFACTORY_H
+
+
+class MeshFactory {
+};
+
+
+#endif //OPENGLDEMO_MESHFACTORY_H
